e10c8df1e9c672b78efec3fec1200ba24217ca9e
mahdi <mahdi.rezayi.853@gmail.com>
2020-11-17 13:08:37 +0330

    removing redux-logger from production build

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-import sessionStorage from 'redux-persist/lib/storage/session';
+import storage from 'redux-persist/lib/storage';
+// import sessionStorage from 'redux-persist/lib/storage/session';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
@@ -11,8 +11,8 @@ import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
     key: 'root',
-    // storage,
-    storage: sessionStorage,
+    storage,
+    // storage: sessionStorage,
     whitelist: ['cart']
 };
 
