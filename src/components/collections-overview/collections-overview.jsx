59720543a06af07e0ddd3db92fe1bba692b74b3b
mahdi <mahdi.rezayi.853@gmail.com>
2020-11-17 12:42:27 +0330

    implementing heroku

---
+++ src/components/collections-overview/collections-overview.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionPreview from '../collection-preview/collection-preview';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import './collections-overview.scss';
+
+
+const CollectionsOverview = ({ collections }) => {
+    // const [collections, setCollections] = useState(SHOP_DATA);
+
+    return (
+        <div className="collections-overview">
+            {/* {
+                collections.map(collection => 
+                    <CollectionPreview 
+                        key={collection.id}
+                        title={collection.title}
+                        items={collection.items}
+                    />
+                )
+            } */}
+            {
+                collections.map(({id, ...otherCollectionItems}) =>
+                    <CollectionPreview 
+                        key={id}
+                        {...otherCollectionItems}
+                    />
+                )
+            }
+        </div>
+    );
+};
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionsForPreview
+});
+
+
+export default connect(mapStateToProps)(CollectionsOverview);
